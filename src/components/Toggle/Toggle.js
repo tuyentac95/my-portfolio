359317e17b338db98786e466714343cbb3e08743
tuyentac95 <tuyentac95@gmail.com>
2022-06-09 22:38:21 +0700

    adding toggle

---
+++ src/components/Toggle/Toggle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Toggle.css';
+import Sun from '@iconscout/react-unicons/icons/uil-sun';
+
+const Toggle = () => {
+  return(
+    <div className="toggle">
+
+    </div>
+  );
+}
+
+export default Toggle;
