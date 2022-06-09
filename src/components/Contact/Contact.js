1c0520eb1a3781994996c209ec8b75cf06922409
tuyentac95 <tuyentac95@gmail.com>
2022-06-09 21:55:03 +0700

    add send email function

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './Contact.css';
 
 const Contact = (props) => {
+
+  const form = useRef();
+  const [done, setDone] = useState(false)
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    console.log(form.current)
+    setDone(true)
+    form.reset();
+  }
+
+
   return (
     <div className="contact-form">
       <div className="w-left">
@@ -14,11 +26,12 @@ const Contact = (props) => {
       </div>
 
       <div className="c-right">
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="user_name" className="user" placeholder="Name"/>
           <input type="email" name="user_mail" className="user" placeholder="Email"/>
           <textarea name="message" className="user" placeholder="Message"/>
           <input type="submit" value="Send" className="button"/>
+          <span>{done && "Thanks for Contacting me!"}</span>
           <div className="blur c-blur1"
                style={{background: "var(--purple)"}}/>
         </form>
