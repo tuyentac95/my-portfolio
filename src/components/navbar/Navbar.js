842ce944a4ca0e8243a54b3a7df14ea5b4bdeb1c
tuyentac95 <tuyentac95@gmail.com>
2022-06-11 20:45:40 +0700

    fix UI

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -30,7 +30,9 @@ const Navbar = () => {
             </Link>
           </ul>
         </div>
-        <button className="button n-button">Contact</button>
+        <Link spy={true} to='Contact' smooth={true}>
+          <button className="button n-button">Contact</button>
+        </Link>
       </div>
     </div>
   );
